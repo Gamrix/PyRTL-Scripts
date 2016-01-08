aaface26f7c21d0ae563f7da3437f41bb04b3f9b
John Clow <johnwclow@hotmail.com>
2016-01-07 19:46:52 -0800

    Some more work on trying to get Travisci working Known to still not work properly

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name = 'pyrtl-scripts',
+    version = '0.0.1',
+    packages =  ['johnlib'],
+    description = 'RTL-level Hardware Design and Simulation Toolkit scripts',
+    author='John Clow',
+    author_email='',
+    install_requires =  ['six', 'pyrtl'],
+    tests_require =  ['tox', 'nose'],
+    extras_require =  {
+        'blif parsing': ['pyparsing']
+        },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: System :: Hardware'
+        ]
+)
